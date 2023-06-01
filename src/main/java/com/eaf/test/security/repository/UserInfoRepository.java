f70806e736be8a8558c5c277e61d00161cfbcdcf
Yogesh More <moreyog@gmail.com>
2023-06-01 20:05:00 +0530

    Initial commit

---
+++ src/main/java/com/eaf/test/security/repository/UserInfoRepository.java
@@ -0,0 +1,11 @@
+package com.eaf.test.security.repository;
+
+import com.eaf.test.security.entity.UserInfo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserInfoRepository extends JpaRepository<UserInfo, Integer> {
+    Optional<UserInfo> findByName(String username);
+
+}
---
+++ src/test/java/com/eaf/test/security/SpringSecurityLatestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eaf.test.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityLatestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
